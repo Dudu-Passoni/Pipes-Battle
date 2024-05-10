a074f28fa379bb407e4ec3f8a44f8a457b87ac1f
Dudu <115907714+Dudu-Passoni@users.noreply.github.com>
2024-05-10 11:29:29 -0300

    Create strings.h

---
+++ strings.h
@@ -0,0 +1,14 @@
+// Descrição: Arquivo de cabeçalho com as definições de constantes utilizadas no programa.
+char info_client[240] = "Client->\n Ataque: \n 1 - Gerar deadlock - 10 de dano - 60 de cahnce de acerto\n 2 - Dividir por zero - 5 de dano - 70 de chance de acerto\n 3 - Jogar um tijolo na CPU - 20 de dano - 40 de chance de acerto \n 4 - Recarregar\n 5 - Sair \n\n";
+char info_server[269] = "Server->\n Ataque: \n 1 - Rodar um programa .exe - 25 de dano - 40 de chance de acerto\n 2 - Abrir Google Chrome no Processo inimigo - 10 de dano - 70 de chance de acerto\n 3 - Abrir Android Studio - 15 de dano - 50 de chance de acerto\n 4 - Recarregar\n 5 - Sair \n\n";
+
+
+char info_attack_1[40] = "Gerando deadlock no processo inimigo\n";
+char info_attack_2[42] = "Dividindo por zero no processo inimigo\n";
+char info_attack_3[47] = "Jogando um tijolo na CPU do processo inimigo\n";
+
+char info_attack_1_server[34] = "Rodando exe no processo inimigo\n";
+char info_attack_2_server[29] = "Abrindo o Google Chrome...\n";
+char info_attack_3_server[45] = "Abrindo Android Studio no processo inimigo\n";
+
+char info_reload[15] = "recarregando\n";
